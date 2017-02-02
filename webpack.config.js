1fd8d30212d555d11e210da7489d2a1f5d607744
Denis Krivichanin <krivichaninds@gmail.com>
2017-02-02 11:10:22 +0300

    Update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,51 @@
 const webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
   entry: [
     'react-hot-loader/patch',
     'babel-polyfill',
-    './app/index',
+    './app/index'
   ],
   output: {
     path: `${__dirname}/dist`,
     filename: 'bundle.js',
-    publicPath: '/dist/',
+    publicPath: '/dist/'
   },
   plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
+    new webpack.NoErrorsPlugin()
   ],
   devServer: {
     port: 8080,
     historyApiFallback: true,
+    compress: true,
+    hot: true,
+    inline: true
   },
   devtool: 'eval-cheap-module-source-map',
   module: {
     preLoaders: [{
       test: /\.js$/,
-      loader: 'source-map-loader',
-      exclude: [
-        /node_modules\/intl-messageformat/,
-        /node_modules\/intl-relativeformat/,
-        /node_modules\/intl-format-cache/,
-      ],
+      loader: 'source-map-loader'
     }],
     loaders: [{
       test: /\.jsx?$/,
-      loader: ['babel-loader'],
+      loader: ['babel'],
       exclude: /node_modules/,
       query: {
-        presets: ['react', 'es2015'],
-      },
+        presets: ['react', 'es2015']
+      }
     }, {
       test: /\.css$/,
-      loader: 'style-loader!css-loader!postcss-loader',
+      loader: 'style-loader!css-loader!postcss-loader'
     }, {
       test: /\.json/,
-      loader: 'json-loader',
+      loader: 'json-loader'
     }, {
       test: /\.(jpe?g|png|gif|svg|eot|woff|ttf|woff2)$/i,
-      loader: 'file',
-    }],
-  },
+      loader: 'file'
+    }]
+  }
 };
