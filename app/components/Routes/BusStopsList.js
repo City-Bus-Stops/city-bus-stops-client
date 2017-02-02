198234c73e44273f126932b58189252e8e34d4a1
Denis Krivichanin <krivichaninds@gmail.com>
2017-02-02 13:58:54 +0300

    Fix route card design

--- app/components/Routes/BusStopsList.js
+++ app/components/Routes/BusStopsList.js
@@ -1,18 +1,34 @@
 import React, { PropTypes } from 'react';
-import { List } from 'semantic-ui-react';
+import { Link } from 'react-router';
+import { Grid, Card } from 'semantic-ui-react';
+import _ from 'lodash';
+
+const getColor = ({ index, length }) => {
+  return _.isEqual(index, 0) ? 'green' : _.isEqual(index, length - 1) ? 'red' : 'blue';
+};
 
 const BusStopList = ({ busStops }) => (
-  <List relaxed>
-    {
-      (busStops.map(busStop =>
-        <List.Item key={busStop.id}>
-          <List.Content>
-            {busStop.name}
-          </List.Content>
-        </List.Item>
-      ))
-    }
-  </List>
+  <Grid columns={3} padded stackable>
+      {
+        (busStops.map((busStop, index) =>
+          <Grid.Column largeScreen="3" mobile="5" widescreen="4" key={busStop.id}>
+            <Card fluid color={getColor({ index, length: busStops.length })} >
+              <Card.Content>
+                <Card.Header>
+                  {busStop.name}
+                </Card.Header>
+                <Card.Description>After {busStop.time}.</Card.Description>
+              </Card.Content>
+              <Card.Content extra>
+                <Link to="/">
+                  Watch bus stop schedule
+                </Link>
+              </Card.Content>
+            </Card>
+          </Grid.Column>
+        ))
+      }
+  </Grid>
 );
 
 BusStopList.propTypes = {
--- app/components/Routes/Route.js
+++ app/components/Routes/Route.js
@@ -1,52 +1,66 @@
 import React, { PropTypes } from 'react';
-import { Card, Grid, Button, Icon, Accordion } from 'semantic-ui-react';
+import { Card, Grid, Button, Icon, Label, Header, Modal } from 'semantic-ui-react';
 
 import BusStopList from './BusStopsList';
 
-const Route = ({ arrivalTime, From, To, points, changeMapState }) => (
+const Route = ({ arrivalTime, From, To, points, changeMapState, busNumber, bustStopName }) => (
   <Grid.Column>
-    <Card
-      fluid
-      color="blue"
-    >
+    <Card fluid color="blue">
       <Card.Content>
-         <Card.Header>
-           {points[0].name}: {arrivalTime}
-           <Button
-             size="medium"
-             icon
-             basic
-             circular
-             floated="right"
-             onClick={() => {
-               changeMapState();
-             }}
-           >
-             <Icon name="street view" color="teal" />
-           </Button>
+        <Card.Header>
+          <Label as="a" color="blue" ribbon size="large" horizontal>
+            {bustStopName}: {arrivalTime}
+          </Label>
+          <Button
+            size="medium"
+            icon
+            basic
+            circular
+            floated="right"
+            onClick={() => {
+              changeMapState();
+            }}
+          >
+            <Icon name="street view" color="teal" />
+          </Button>
+          <Header as="h4" dividing color="grey">
+            Bus number - {busNumber}
+          </Header>
         </Card.Header>
+        <Card.Description>
+          {`${From} - ${To}`}
+        </Card.Description>
       </Card.Content>
-      <Card.Content
-        meta={
-          <p>
-            {`${From} - ${To}`}
-          </p>
-        }
-      />
-      <Card.Content extra className="card-extra-component">
-        <Accordion>
-          <Accordion.Title>
+      <Card.Content extra>
+        <Modal
+          trigger={
             <a>
               <Icon name="expand" />
               Show bus stops
             </a>
-          </Accordion.Title>
-          <Accordion.Content>
-            <Card.Content
-              description={points && <BusStopList busStops={points} />}
-            />
-          </Accordion.Content>
-        </Accordion>
+          }
+          closeIcon="close"
+          dimmer="blurring"
+          closeOnEscape={true}
+          closeOnRootNodeClick={false}
+        >
+          <Modal.Header>
+            <Header as="h1" color="blue">
+              {bustStopName}
+            </Header>
+          </Modal.Header>
+          <Modal.Content>
+            <Header as="h2" color="blue">
+              Bus number - {busNumber}
+              <Header.Subheader>
+                Bus stops count: <strong>{points.length}</strong>
+              </Header.Subheader>
+            </Header>
+            <Modal.Description>
+              {points && <BusStopList busStops={points} />}
+            </Modal.Description>
+          </Modal.Content>
+        </Modal>
       </Card.Content>
     </Card>
   </Grid.Column>
